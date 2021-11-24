8d6a274958aa06950c743d812924c294dd8eb4bf
Galang aidil akbar <galangaidil.akbar@student.uty.ac.id>
2021-11-24 10:11:42 +0700

    fix: mix issue in https

--- routes/auth.php
+++ routes/auth.php
@@ -10,6 +10,10 @@ use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
+if (App::environment('production')) {
+    URL::forceScheme('https');
+}
+
 Route::get('/register', [RegisteredUserController::class, 'create'])
                 ->middleware('guest')
                 ->name('register');
