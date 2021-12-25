2266b56a36d069c325b8349429cdb3d6692007dc
Galang Aidil Akbar <galangaidil.akbar@student.uty.ac.id>
2021-12-25 08:44:29 +0700

    add: active chapter link and fix: animate pulse

--- resources/views/courses/selected.blade.php
+++ resources/views/courses/selected.blade.php
@@ -2,20 +2,27 @@
     <div class="py-1 bg-white">
         <div class="max-w-7xl mx-auto">
             <div class="flex flex-col-reverse lg:grid lg:grid-cols-6 gap-4 p-6">
-                <div class="lg:col-span-1">
+
+                {{-- Sidebar --}}
+                <div class="bg-gray-100 rounded-md lg:!bg-white lg:col-span-1">
                     <h3 class="text-gray-900 text-xl font-light text-center lg:text-left px-3">Chapter</h3>
                     <ul class="py-2">
                         @forelse ($course->detailCourse as $item)
-                        <li class="block text-gray-500 hover:text-blue-500 hover:bg-blue-100 rounded text-lg py-1 px-3">
-                            <a href="/courses/{{ $course->slug }}/{{ $item->slug }}">{{ $loop->index + 1 }} {{
-                                $item->chapter }}</a>
+                        <li class="block text-gray-500 hover:text-blue-500 hover:bg-blue-100 {{ Request::is('courses/'.$course->slug.'/'.$item->slug) ? 'text-blue-500 !bg-blue-100' : '' }} rounded text-lg py-1 px-3 mb-1">
+                            <a href="/courses/{{ $course->slug }}/{{ $item->slug }}">
+                                {{ $loop->index + 1 }}
+                                {{ $item->chapter }}
+                            </a>
                         </li>
                         @empty
                         <p>No modul found</p>
                         @endforelse
                     </ul>
                 </div>
+
+                {{-- Content --}}
                 <div class="lg:col-span-5">
+                    {{-- Main Content --}}
                     @if (Request::is('courses/'.$course->slug))
                     <header class="mb-5">
                         <h1 class="inline-block text-3xl font-extrabold text-gray-900">{{ $course->judul }}</h1>
@@ -28,7 +35,8 @@
                     <x-video>
                         {{ $course->video }}
                     </x-video>
-                    
+
+                    {{-- Secondary Content --}}
                     @else
                     <header class="mb-5">
                         <h1 class="inline-block text-3xl font-extrabold text-gray-900">{{ $data->chapter }}</h1>
