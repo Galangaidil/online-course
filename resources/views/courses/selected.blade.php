d5741b9527f19dc7520fb2e960b24de4ec9d64eb
Galang Aidil Akbar <73085251+Galangaidil@users.noreply.github.com>
2022-01-06 18:07:48 +0700

    improve: code maintaining

--- resources/views/courses/selected.blade.php
+++ resources/views/courses/selected.blade.php
@@ -8,15 +8,15 @@
                     <h3 class="text-gray-900 text-xl font-light text-center lg:text-left px-3">Chapter</h3>
                     <ul class="py-2">
                         @forelse ($course->detailCourse as $item)
-                        <li>
-                            <a class="block text-gray-500 hover:text-blue-500 hover:bg-blue-100 {{ Request::is('courses/'.$course->slug.'/'.$item->slug) ? 'text-blue-500 !bg-blue-100' : '' }} rounded text-lg py-1 px-3 mb-1"
-                                href="/courses/{{ $course->slug }}/{{ $item->slug }}">
-                                {{ $loop->index + 1 }}
-                                {{ $item->chapter }}
-                            </a>
-                        </li>
+                            <li>
+                                <a class="block text-gray-500 hover:text-blue-500 hover:bg-blue-100 {{ Request::is('courses/' . $course->slug . '/' . $item->slug) ? 'text-blue-500 !bg-blue-100' : '' }} rounded text-lg py-1 px-3 mb-1"
+                                    href="/courses/{{ $course->slug }}/{{ $item->slug }}">
+                                    {{ $loop->index + 1 }}
+                                    {{ $item->chapter }}
+                                </a>
+                            </li>
                         @empty
-                        <p>No modul found</p>
+                            <p>No modul found</p>
                         @endforelse
                     </ul>
                 </div>
@@ -24,28 +24,28 @@
                 {{-- Content --}}
                 <div class="lg:col-span-5">
                     {{-- Main Content --}}
-                    @if (Request::is('courses/'.$course->slug))
-                    <header class="mb-5">
-                        <h1 class="inline-block text-3xl font-extrabold text-gray-900">{{ $course->judul }}</h1>
-                    </header>
+                    @if (Request::is('courses/' . $course->slug))
+                        <header class="mb-5">
+                            <h1 class="inline-block text-3xl font-extrabold text-gray-900">{{ $course->judul }}</h1>
+                        </header>
 
-                    <p class="mt-2 text-lg text-gray-700">
-                        {{ $course->deskripsi }}
-                    </p>
+                        <p class="mt-2 text-lg text-gray-700">
+                            {{ $course->deskripsi }}
+                        </p>
 
-                    <x-video>
-                        {{ $course->video }}
-                    </x-video>
+                        <x-video>
+                            {{ 'https://www.youtube.com/embed/' . $course->video }}
+                        </x-video>
 
-                    {{-- Secondary Content --}}
+                        {{-- Secondary Content --}}
                     @else
-                    <header class="mb-5">
-                        <h1 class="inline-block text-3xl font-extrabold text-gray-900">{{ $data->chapter }}</h1>
-                    </header>
+                        <header class="mb-5">
+                            <h1 class="inline-block text-3xl font-extrabold text-gray-900">{{ $data->chapter }}</h1>
+                        </header>
 
-                    <x-video>
-                        {{ $data->video }}
-                    </x-video>
+                        <x-video>
+                            {{ $data->video }}
+                        </x-video>
                     @endif
 
                 </div>
